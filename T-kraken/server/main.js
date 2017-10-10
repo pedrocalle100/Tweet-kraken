4d653a3d91068fcdaa34fc269ca8f9faadd6c9b8
jbecerr2 <j_mbc95@hotmail.com>
2017-10-09 20:16:41 -0500

    inicialización de funciones para el llamado de trending topics (DOESNT WORK FOR NOW)

--- T-kraken/server/main.js
+++ T-kraken/server/main.js
@@ -1,12 +1,14 @@
 import { Meteor } from 'meteor/meteor';
 import Twit from Meteor.npmRequire("twit");
-
+let Twit = Meteor.npmRequire("twit");
+let TrendingRegion = Meteor.npmRequire(/trends/place.json);
 let twit = null;
 
 Meteor.startup(() => {
-  // code to run on server at startup
-console.log("Server Init");
-twit = twitterAppInit();
+  console.log("Server Init");
+  twit = twitterAppInit();
+  //getTrending({some stuff})
+  //
 });
 
 function twitterAppInit(){
@@ -16,8 +18,23 @@ function twitterAppInit(){
       consumer_secret:'l8rarzZJUKAuo7JkLGv1QK81w6eRQXxSNJsUvoCAXvFXRhCsQ2',
       access_token:'917107049984610304-E0EojXT6kyRQV5qVVPouq6Ysc18dH8x',
       access_token_secret:'Qu4TYgKCzLEDea15wQ79803LNvmkXGqhDnzY09X3va7Av'
+
   });
 
     return twit;
+
+    /*
+    function getTrending(params){
+      let trending = new TrendingRegion
+      twit.post("some stuff", params, function(err, data, response){
+        trending.return({
+          err:err,
+          res:data
+        })
+      })
+      return trending.wait();
+
+    }
+    /*
 
 }
